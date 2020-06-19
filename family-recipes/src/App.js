791bfc1bcef6c46fd10b755ae8b97726e6e4d54f
unknown <rinzaru@gmail.com>
2020-06-18 19:31:50 -0600

    recipe form

--- family-recipes/src/App.js
+++ family-recipes/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 // import Login from "./components/login";
-import Register from './components/register'
+// import Register from './components/register'
+import RecipeCard from "./components/recipe";
+//   <Register />
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <RecipeCard />
     </div>
   );
 }
---
+++ family-recipes/src/components/recipe.js
@@ -0,0 +1,65 @@
+import React from "react";
+import {
+  Card,
+  Form,
+  Button,
+  FormGroup,
+  Label,
+  Input,
+  textarea,
+} from "reactstrap";
+
+const RecipeCard = () => {
+  return (
+    <Card style={{ margin: "20px auto", width: "50%" }}>
+      <Form style={{ margin: "20px auto" }}>
+        <FormGroup>
+          <Label for="recipeTitle">Recipe Title</Label>
+          <Input
+            type="text"
+            name="recipeTitle"
+            id="recipeTitle"
+            placeholder="Title"
+          />
+          <FormGroup>
+            <Label for="source">Source</Label>
+            <Input type="text" name="source" id="source" placeholder="source" />
+          </FormGroup>
+        </FormGroup>
+        <FormGroup>
+          <Label for="Ingredients">Ingredients</Label>
+          <Input
+            type="textarea"
+            name="Ingredients"
+            id="Ingredients"
+            placeholder="Ingredients"
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="instructions">instructions</Label>
+          <Input
+            type="textarea"
+            name="instructions"
+            id="instructions"
+            placeholder="instructions"
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="catergories">Catergories</Label>
+          <Input
+            type="text"
+            name="catergories"
+            id="catergories"
+            placeholder="catergories"
+          />
+        </FormGroup>
+
+        <Button color="danger">Add Recipe</Button>
+      </Form>
+    </Card>
+  );
+};
+
+export default RecipeCard;

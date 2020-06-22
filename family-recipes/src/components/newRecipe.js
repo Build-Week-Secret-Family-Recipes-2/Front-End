98f3587c64aed06cd973feaba5d7c285836a1988
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-06-21 22:09:08 -0400

    newrecipe setup

---
+++ family-recipes/src/components/newRecipe.js
@@ -0,0 +1,16 @@
+// import React, { useState, useEffect } from 'react'
+// import axios from 'axios';
+// import authentication from '../utils/authentication';
+
+// const  NewRecipe = ()=> {
+
+// return (
+//     <div>
+//         <h2>New Recipe</h2>
+//     </div>
+//  //FORM STUFF
+// )
+
+// }
+
+// export default NewRecipe;

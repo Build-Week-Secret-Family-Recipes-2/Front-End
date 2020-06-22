4fa662333aa29a9d94cfc18e810ed24f406e9340
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-06-21 22:36:22 -0400

    imports added to app.js

--- family-recipes/src/App.js
+++ family-recipes/src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 // import Login from "./components/login";
 import Register from "./components/register";
 // import RecipeCard from "./components/recipe";
-//
+// import PrivateRoute from "./utils/privateRoute"
+// import { BrowserRouter as Router, Route, useHistory} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
+      {/* <Route path="/register" component={Register} /> */}
       <Register />
     </div>
   );

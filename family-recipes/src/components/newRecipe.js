3fb8ea2157ff7fa6faf275a31156a3cf8cf9dfa3
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-06-21 22:18:09 -0400

    newrecipe outline updated

--- family-recipes/src/components/newRecipe.js
+++ family-recipes/src/components/newRecipe.js
@@ -2,7 +2,24 @@
 // import axios from 'axios';
 // import authentication from '../utils/authentication';
 
+
+// const initialRecipe = {
+//     recipeTitle: "",
+//     source: "",
+//     ingredients: "",
+//     instructions: "",
+//     categories: "",
+// }
+
 // const  NewRecipe = ()=> {
+
+// const handleChange = e => {
+
+// }
+
+// const handleSubmit = e => {
+
+// }
 
 // return (
 //     <div>

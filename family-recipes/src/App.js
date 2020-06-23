4976df790c5620f2033cba940455537619031e47
unknown <rinzaru@gmail.com>
2020-06-22 20:09:15 -0600

    dasdsad

--- family-recipes/src/App.js
+++ family-recipes/src/App.js
@@ -1,32 +1,30 @@
 import React from "react";
 // import Login from "./components/login";
-import Register from "./components/register";
-// import RecipeCard from "./components/recipe";
+// import Register from "./components/register";
+import RecipeCard from "./components/recipe";
 // import NewRecipe from "./components/newRecipe"
 // import PrivateRoute from "./utils/privateRoute"
 // import { Route, Switch} from "react-router-dom";
 
-
 function App() {
-
   return (
     <div className="App">
-          {/* <Switch> */}
-          {/* <Route path='/register' >
+      {/* <Switch> */}
+      {/* <Route path='/register' >
             <Register />
           </Route> */}
 
-          {/* <Route exact path='/login'>
+      {/* <Route exact path='/login'>
             <Login />
           </Route> */}
 
-          {/* <PrivateRoute path='BACK END API PATH' component={NewRecipe} />
+      {/* <PrivateRoute path='BACK END API PATH' component={NewRecipe} />
 
           <PrivateRoute path='/' component={RecipeCard} /> */}
 
-        {/* </Switch> */}
-      
-      <Register/>
+      {/* </Switch> */}
+
+      <RecipeCard />
     </div>
   );
 }

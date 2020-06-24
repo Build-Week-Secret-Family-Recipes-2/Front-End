e59c466b1f6805f661f5974dc4f0250e8cf67bca
sofia Luna <Sofia21luna@gmail.com>
2020-06-23 22:26:04 -0400

    Route Management complete

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React, { useState, useEffect } from "react";
-// import Login from "./components/login";
-// import Register from "./components/register";
+import React from "react";
+import Login from "./components/login";
+import Register from "./components/register";
 import RecipeCard from "./components/recipe";
-// import NewRecipe from "./components/newRecipe"
-// import PrivateRoute from "./utils/privateRoute"
-// import { Route, Switch} from "react-router-dom";
+import NewRecipe from "./components/newRecipe"
+import PrivateRoute from "./utils/privateRoute"
+import { Route, Switch} from "react-router-dom";
 // import UserRecipes from "./components/userRecipes";
-import axios from "axios";
-import Users from './components/users';
+import Users from "./components/users";
 
 function App() {
   return (
     <div className="App">
-      {/* <Switch> */}
-      {/* <Route path='/register' >
-            <Register />
-          </Route> */}
+      <Switch>
+        <Route path="/register">
+          <Register />
+        </Route>
 
-      {/* <Route exact path='/login'>
-            <Login />
-          </Route> */}
+        <Route exact path="/login">
+          <Login />
+        </Route>
 
-      {/* <PrivateRoute path='BACK END API PATH' component={NewRecipe} />
+        <PrivateRoute path="BACK END API PATH" component={NewRecipe} />
 
-          <PrivateRoute path='/' component={RecipeCard} /> */}
+        <PrivateRoute path="/" component={RecipeCard} />
 
-      {/* </Switch> */}
-
-      <Users />
+        <Route exact path='/users'>
+        <Users />
+        </Route>
+      </Switch>
     </div>
   );
 }
